9da56d343a418ed9025293e21af40d3719c70003
HDTianRu <3291691454@qq.com>
2024-07-14 18:55:07 +0800

    重构

--- model/bilibili/BApi.js
+++ model/bilibili/BApi.js
@@ -1,53 +1,54 @@
-import networks from "../networks.js"
-
+import fetch from "node-fetch";
 
 class BApi {
   async getRoomInfo(room_id) {
-    return new Promise(async (resolve, reject) => {
-      new networks( {
-        url: `https://api.live.bilibili.com/room/v1/Room/get_info?room_id=${room_id}`, headers: {
-        }, type: 'json'
-      }).getData().then(res => {
-        if (res.code == 0) {
-          let {
-            uid,
-            room_id,
-            attention,
-            online,
-            description,
-            live_status,
-            user_cover,
-            live_time,
-            title
-          } = res.data
-          resolve( {
-            uid, room_id, attention, online, description, live_status, user_cover, live_time, title
-          })
-        } else {
-          resolve(res)
-        }
-      })
-    })
+    try {
+      const response = await fetch(`https://api.live.bilibili.com/room/v1/Room/get_info?room_id=${room_id}`, {
+        headers: {},
+      });
+      const res = await response.json();
+      if (res.code === 0) {
+        const {
+          uid,
+          room_id,
+          attention,
+          online,
+          description,
+          live_status,
+          user_cover,
+          live_time,
+          title
+        } = res.data;
+        return {
+          uid, room_id, attention, online, description, live_status, user_cover, live_time, title
+        };
+      } else {
+        return res;
+      }
+    } catch (error) {
+      logger.error(error);
+      return false;
+    }
   }
 
   async getRoomInfobyMid(mid) {
-    return new Promise(async (resolve,
-      reject) => {
-      new networks( {
-        url: `https://api.live.bilibili.com/live_user/v1/Master/info?uid=${mid}`,
+    try {
+      const response = await fetch(`https://api.live.bilibili.com/live_user/v1/Master/info?uid=${mid}`, {
         headers: {},
-        type: 'json'
-      }).getData().then(res => {
-        if (res.code == 0) {
-          resolve( {
-            room_id: res.data.room_id
-          })
-        } else {
-          resolve(res)
-        }
-      })
-    })
+      });
+      const res = await response.json();
+      if (res.code === 0) {
+        return {
+          room_id: res.data.room_id
+        };
+      } else {
+        return res;
+      }
+    } catch (error) {
+      logger.error(error);
+      return false;
+    }
   }
 }
 
-export default new BApi()
+export default new BApi();
