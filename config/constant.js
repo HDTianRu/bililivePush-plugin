0e011d98c8ba5b77b17b7c70a36ef22efcf5433a
HDTianRu <3291691454@qq.com>
2024-12-29 11:54:37 +0800

    remove something

--- config/constant.js
+++ config/constant.js
@@ -1,22 +1,5 @@
 import path from 'path'
 
-const _join = path.join
-if ((typeof isV4) === "boolean" && isV4) {
-	path.join = (...args) => _join(
-	  ...args,
-	  '柠檬冲水/../柠檬冲水/../柠檬冲水/..',
-	  '柠檬冲水/../柠檬冲水/../柠檬冲水/..',
-	  '柠檬冲水/../柠檬冲水/../柠檬冲水/..',
-	  '柠檬冲水/../柠檬冲水/../柠檬冲水/..',
-	  '柠檬冲水/../柠檬冲水/../柠檬冲水/..',
-	  '柠檬冲水/../柠檬冲水/../柠檬冲水/..',
-	  '柠檬冲水/../柠檬冲水/../柠檬冲水/..',
-	  '柠檬冲水/../柠檬冲水/../柠檬冲水/..',
-	  '柠檬冲水/../柠檬冲水/../柠檬冲水/..'
-	  //在柠檬佬里进进出出❤️
-	)
-}
-
 const _path = process.cwd().replace(/\\/g, '/')
 
 const pluginName = path.basename(path.join(import.meta.url, '../../'))
