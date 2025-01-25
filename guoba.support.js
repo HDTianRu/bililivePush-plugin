51ccb9c4b8306eebe6391a5efedc554baef3233c
HDTianRu <3291691454@qq.com>
2025-01-25 23:22:37 +0800

    添加锅巴支持

---
+++ guoba.support.js
@@ -0,0 +1,40 @@
+import path from 'path'
+import lodash from 'lodash'
+import Cfg from './model/Cfg.js'
+
+export function supportGuoba() {
+  return {
+    pluginInfo: {
+      name: 'bililivePush-plugin',
+      title: 'bililivePush-plugin',
+      author: '@天如',
+      authorLink: 'https://gitee.com/HDTianRu',
+      link: 'https://gitee.com/HDTianRu/bililivePush-plugin',
+      isV3: true,
+      isV2: false,
+      description: '当然是推送b站直播啦',
+      icon: 'mdi:stove',
+      iconColor: '#6bb9dd',
+    },
+    configInfo: {
+      schemas: [{
+        field: 'user.sleep',
+        label: '群发间隔',
+        bottomHelpMessage: '推送给多个群时间隔的时间(单位秒)',
+        component: 'input',
+      }],
+      getConfigData() {
+        return Cfg.merge()
+      },
+      setConfigData(data, {
+        Result
+      }) {
+        for (let [keyPath, value] of Object.entries(data)) {
+          Cfg._set(keyPath, value)
+        }
+        Cfg.fresh()
+        return Result.ok({}, '保存成功~')
+      },
+    },
+  }
+}
---
+++ model/Cfg.js
@@ -0,0 +1,40 @@
+import fs from "fs"
+import lodash from "lodash"
+import path from "path"
+import {
+  pluginRoot
+} from "../config/constant.js"
+
+const _cfgPath = path.join(pluginRoot, "data")
+let cfg = {}
+
+try {
+  if (fs.existsSync(_cfgPath + "cfg.json")) {
+    cfg = JSON.parse(fs.readFileSync(path.join(_cfgPath, "cfg.json"), "utf8")) || {}
+  } else {
+    cfg = JSON.parse(fs.readFileSync(path.join(_cfgPath, "cfg_default.json"), "utf8")) || {}
+  }
+} catch (e) {
+  // do nth
+}
+
+const Cfg = {
+  get(rote, def = '') {
+    return lodash.get(cfg, rote, def)
+  },
+  set(rote, val) {
+    Cfg._set(rote, val)
+    Cfg.fresh()
+  },
+  _set(rote, val) {
+    lodash.set(cfg, rote, val)
+  },
+  fresh() {
+    fs.writeFileSync(_cfgPath + "cfg.json", JSON.stringify(cfg, null, "\t"))
+  },
+  merge() {
+    return lodash.merge({}, cfg)
+  }
+}
+
+export default Cfg
