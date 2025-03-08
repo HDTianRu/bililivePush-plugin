af6ab5a67c4dbf919427abeb241291cb4700d2d4
HDTianRu <3291691454@qq.com>
2025-03-08 10:09:53 +0800

    fix

--- model/Cfg.js
+++ model/Cfg.js
@@ -9,15 +9,19 @@ const _cfgPath = path.join(pluginRoot, "data")
 let _cfg = {}
 
 try {
-  _cfg.user = JSON.parse(fs.readFileSync(path.join(_cfgPath, "cfg.json"), "utf8")) || {}
   _cfg.def = JSON.parse(fs.readFileSync(path.join(_cfgPath, "cfg_default.json"), "utf8")) || {}
+  if (fs.existsSync(path.join(_cfgPath, "cfg.json"))) {
+    _cfg.user = JSON.parse(fs.readFileSync(path.join(_cfgPath, "cfg.json"), "utf8")) || {}
+  } else {
+    _cfg.user = {}
+  }
 } catch (e) {
-  loggor.warn("读取配置文件失败", e)
+  logger.warn("读取配置文件失败", e)
 }
 
 const Cfg = {
   get(rote, def = undefined) {
-    return lodash.get(_cfg.user, rote) || lodash.get(_cfg.def, rote) || def
+    return lodash.get(_cfg.user, rote) ?? lodash.get(_cfg.def, rote) ?? def
   },
   set(rote, val) {
     Cfg._set(rote, val)
@@ -30,7 +34,7 @@ const Cfg = {
     fs.writeFileSync(path.join(_cfgPath, "cfg.json"), JSON.stringify(_cfg.user, null, "\t"))
   },
   merge() {
-    return lodash.merge({}, _cfg.def, _cfg.user)
+    return lodash.merge(_cfg.user, _cfg.def)
   }
 }
 
