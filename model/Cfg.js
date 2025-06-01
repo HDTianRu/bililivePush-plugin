dccbf5ab97a65a2733fa3c6fa359ea06421c628c
HDTianRu <3291691454@qq.com>
2025-06-01 20:30:13 +0800

    细节优化

--- model/Cfg.js
+++ model/Cfg.js
@@ -1,41 +1,74 @@
 import fs from "fs"
 import lodash from "lodash"
-import path from "path"
+import {
+  join
+} from "path"
 import {
   pluginRoot
 } from "../config/constant.js"
 
-const _cfgPath = path.join(pluginRoot, "data")
-let _cfg = {}
+const _cfgPath = join(pluginRoot, "data")
+let cfg = {}
 
-try {
-  _cfg.def = JSON.parse(fs.readFileSync(path.join(_cfgPath, "cfg_default.json"), "utf8")) || {}
-  if (fs.existsSync(path.join(_cfgPath, "cfg.json"))) {
-    _cfg.user = JSON.parse(fs.readFileSync(path.join(_cfgPath, "cfg.json"), "utf8")) || {}
-  } else {
-    _cfg.user = {}
+const loadConfig = () => {
+  let defCfg = {}
+  let userCfg = {}
+  try {
+    defCfg = JSON.parse(fs.readFileSync(join(_cfgPath, "cfg_default.json"), "utf8")) || {}
+    if (fs.existsSync(join(_cfgPath, "cfg.json"))) {
+      userCfg = JSON.parse(fs.readFileSync(join(_cfgPath, "cfg.json"), "utf8")) || {}
+    }
+  } catch (e) {
+    logger.warn("读取配置文件失败", e)
   }
-} catch (e) {
-  logger.warn("读取配置文件失败", e)
+  cfg = lodash.merge({}, defCfg, userCfg)
+  if (cfg.special?.[0]) {
+    cfg.override = Object.fromEntries(
+      cfg.special.map(({
+        key,
+        ...rest
+      }) => [key, rest])
+    )
+  }
+}
+
+loadConfig()
+
+const watcher = (type) => (type === 'change') ? setTimeout(loadConfig, 1000) : null
+
+fs.watch(join(_cfgPath, "cfg_default.json"), watcher)
+
+if (fs.existsSync(join(_cfgPath, "cfg.json"))) {
+  fs.watch(join(_cfgPath, "cfg.json"), watcher)
 }
 
 const Cfg = {
-  get(rote, def = undefined) {
-    return lodash.get(_cfg.user, rote) ?? lodash.get(_cfg.def, rote) ?? def
+  get(rote, def, e) {
+    if (!e?.group_id || !cfg.special) return lodash.get(cfg, rote, def)
+    const override = lodash.get(cfg, `override.${e.self_id}:${e.group_id}`) ??
+      lodash.get(cfg, `override.*:${e.group_id}`) ??
+      lodash.get(cfg, `override.${e.self_id}:*`) ?? {}
+    return lodash.get(override, rote) ?? lodash.get(cfg, rote, def)
   },
   set(rote, val) {
     Cfg._set(rote, val)
     Cfg.save()
   },
   _set(rote, val) {
-    lodash.set(_cfg.user, rote, val)
+    lodash.set(cfg, rote, val)
   },
   save() {
-    fs.writeFileSync(path.join(_cfgPath, "cfg.json"), JSON.stringify(_cfg.user, null, "\t"))
+    try {
+      fs.writeFileSync(join(_cfgPath, "cfg.json"), JSON.stringify(Cfg.getAll(), null, "\t"))
+    } catch (e) {
+      logger.warn("保存配置文件失败", e)
+    }
   },
-  merge() {
-    return lodash.merge(_cfg.user, _cfg.def)
-  }
+  getAll() {
+    let data = lodash.cloneDeep(cfg)
+    delete data["override"]
+    return data
+  },
 }
 
 export default Cfg
