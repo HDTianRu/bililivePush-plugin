ddb2ae81ce4f401d23bd3c35cfb036f8788b3ace
HDTianRu <3291691454@qq.com>
2025-06-01 20:36:45 +0800

    细节优化

--- guoba.support.js
+++ guoba.support.js
@@ -35,7 +35,7 @@ export function supportGuoba() {
         component: "Switch"
       }],
       getConfigData() {
-        return Cfg.merge()
+        return Cfg.getAll()
       },
       setConfigData(data, {
         Result
